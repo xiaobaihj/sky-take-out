7d9fafef0fea6d2de5f0dc8579655547de110ed2
hzg <15920371+hzg2137386613@user.noreply.gitee.com>
2025-09-25 14:05:30 +0800

    删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -99,10 +99,20 @@ public class DishServiceImpl implements DishService {
         }
 
         //3、删除菜品表中的菜品数据
-        for (Long id : ids) {
-            dishMapper.deleteById(id);
-            //4、删除菜品的口味数据
-            dishFlavorMapper.deleteByDishId(id);
-        }
+//        for (Long id : ids) {
+//            dishMapper.deleteById(id);
+//            //4、删除菜品的口味数据
+//            dishFlavorMapper.deleteByDishId(id);
+//        }
+
+        //优化：因为for循环里面发送了两条sql语句，所以当删除大量的菜品的时候，可能会导致性能方面的问题
+        //sql: delete from dish where id in (?,?,?)
+        //根据菜品id集合批量删除菜品数据
+        dishMapper.deleteByIds(ids);
+
+        //根据菜品id集合批量删除关联的口味数据
+        //sql: delete from dish_flavor where dish_id in (?,?,?)
+        dishFlavorMapper.deleteByIds(ids);
+
     }
 }
